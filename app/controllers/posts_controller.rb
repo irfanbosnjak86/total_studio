c99662a58e91f96ac8c3e1ec8b8718813fb43062
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2015-12-07 05:05:12 +0100

    Add new column to Posts Model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:title, :description, post_images_attributes: [ :caption, :id, :photo, :_destroy ])
+		params.require(:post).permit(:title, :description, :category_name, post_images_attributes: [ :caption, :id, :photo, :_destroy ])
 	end
 
 	def find_posts
---
+++ db/migrate/20151207032313_add_category_to_posts.rb
@@ -0,0 +1,5 @@
+class AddCategoryToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :category_name, :string
+  end
+end
