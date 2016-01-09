a0ce94edd024b088ee212ef4a4593d3553d724a9
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-01-09 20:59:13 +0100

    Added Carousel to Slider#Index

--- app/controllers/sliders_controller.rb
+++ app/controllers/sliders_controller.rb
@@ -2,7 +2,7 @@ class SlidersController < ApplicationController
   before_action :find_slider, only: [:show, :edit, :update, :destroy]
   layout "application"
   def index
-    @slider = Slider.all
+    @slider = Slider.find(2)
   end
 
   def show
--- app/models/slider_image.rb
+++ app/models/slider_image.rb
@@ -1,9 +1,6 @@
 class SliderImage < ActiveRecord::Base
   belongs_to :slider
 
-  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100>" },
-  :storage => :dropbox,
-  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-  :dropbox_options => { :path => proc { |style| "images/#{id}/#{style}/#{image.original_filename}" } }
+  has_attached_file :image, styles: { medium: "1920x1282#", thumb: "100x100>" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
