bfa0e3f89a397226156e231153f610b9860d5f50
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-01-18 15:17:14 +0100

    Edited Slider controler for Dropbox upload

--- app/models/slider_image.rb
+++ app/models/slider_image.rb
@@ -1,6 +1,9 @@
 class SliderImage < ActiveRecord::Base
   belongs_to :slider
 
-  has_attached_file :image, styles: { medium: "1920x1282#", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "1920x1282#", thumb: "100x100>" },
+  :storage => :dropbox,
+  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  :dropbox_options => { :path => proc { |style| "images/#{id}/#{style}/#{image.original_filename}" } }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
