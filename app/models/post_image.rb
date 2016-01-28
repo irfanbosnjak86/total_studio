b297b8679a6a49ca005362e77f480e4c76f5e602
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-01-28 03:51:00 +0100

    Eddited model images fo Heroku Deploy

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,9 @@
 class PostImage < ActiveRecord::Base
   belongs_to :post
 
-  has_attached_file :photo, styles: { medium: "800x640>", index: "355x355#", thumb: "100x100>" }
+  has_attached_file :photo, styles: { medium: "856x570>", index: "355x355#", thumb: "100x100>" },
+  :storage => :dropbox,
+  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  :dropbox_options => { :path => proc { |style| "images/#{id}/#{style}/#{photo.original_filename}" } }
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
