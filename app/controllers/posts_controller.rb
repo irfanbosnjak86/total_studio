51e5f7969392686c8853586a090ecf588f5894a6
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-01-28 03:45:14 +0100

    Eddited Kaminari Prev/Next links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
 
   def index
     if params[:category_name]
-      @posts = Post.where(:category_name => params[:category_name]).order("created_at DESC").page(params[:page]).per(8)
+      @posts = Post.where(:category_name => params[:category_name]).order("created_at DESC").page(params[:page]).per(3)
     else
-      @posts = Post.all.order("created_at DESC").page(params[:page]).per(8)
+      @posts = Post.all.order("created_at DESC").page(params[:page]).per(3)
     end
   end
 
