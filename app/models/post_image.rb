cd952649b97cd0e33bd4686bd104239769e3be36
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-02-27 03:32:25 +0100

    Added validation to :photo and omited lines for Dropbox (working in local)

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,5 @@ class PostImage < ActiveRecord::Base
 
   has_attached_file :photo, styles: { medium: "739x493>", index: "300x300#", thumb: "100x100>" }
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+    validates :photo, presence: true
 end
