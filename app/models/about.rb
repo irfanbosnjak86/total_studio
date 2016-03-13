0aeb3e2ffd290b71e757fb907b11a3b2057d52d3
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-03-14 00:36:25 +0100

    Edited models for Heroku

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,6 +1,9 @@
 class About < ActiveRecord::Base
   belongs_to :admin
 
-  has_attached_file :photo, styles: { medium: "526x365>" }
+  has_attached_file :photo, styles: { medium: "526x365>" }, 
+  :storage => :dropbox,
+  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  :dropbox_options => { :path => proc { |style| "images/#{id}/#{style}/#{photo.original_filename}" } }
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,10 @@
 class PostImage < ActiveRecord::Base
   belongs_to :post
 
-  has_attached_file :photo, styles: { medium: "739x493>", index: "300x300#", thumb: "100x100>" }
+  has_attached_file :photo, styles: { medium: "739x493>", index: "300x300#", thumb: "100x100>" },
+  :storage => :dropbox,
+  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  :dropbox_options => { :path => proc { |style| "images/#{id}/#{style}/#{photo.original_filename}" } }
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
     validates :photo, presence: true
 
