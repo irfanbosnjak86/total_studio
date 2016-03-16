c1f5b0fdb139a13568d378dcd11de8ecf98c3724
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-03-16 23:56:43 +0100

    Cleaned unnecessary files, edited post.coffe file not to use fileupload function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  @about = About.create 
+  @slider = Slider.create
+  @contact = Contact.create
