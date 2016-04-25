b51fd019af02c75540ab0b378f5583855a75360e
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2016-04-25 23:58:24 +0200

    Ordering images and videos by created_at

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,6 @@
 class PostImage < ActiveRecord::Base
   belongs_to :post
+  default_scope { order('created_at ASC') }
 
   has_attached_file :photo, styles: { index: "300x300#" }
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < ActiveRecord::Base
   belongs_to :post
+  default_scope { order('created_at ASC') }
 end
